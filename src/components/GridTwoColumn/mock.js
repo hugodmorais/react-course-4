db23d5309dfbd7d25003189cc442f4c9b5b323c5
Hugo Morais <hugo.morais@frontkom.com>
2022-05-09 14:53:38 +0100

    GridTwoColumn component

---
+++ src/components/GridTwoColumn/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid two columns',
+  text: `Literum ipsum dolor sit amet consectetur adipisicing elit. Will there be a great difference between the guilt of these pains? From her needs? Are the accusers suing that, by the right of mind, that pain is something or a great pain ?`,
+  srcImg: 'assets/images/javascript.svg',
+};
